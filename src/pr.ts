ed3a3086df0701a4ff82c8972a1d9da44ba9fb95
Brannon Lucas <blucas@aumni.fund>
2023-02-09 10:40:06 -0700

    Add comment to test jira action

--- src/pr.ts
+++ src/pr.ts
@@ -7,6 +7,7 @@ import {getInput, Options} from './options'
 export async function process(
   context: Context,
   isValid = validate
+  // testing
 ): Promise<void> {
   if (context.eventName !== 'pull_request') {
     core.debug('Not a pull request')
